201ce57a459ef256f765a514fa956ae6d3e06be7
Sergey Parshin <quarck@gmail.com>
2020-05-27 00:58:30 +0100

    Simplify all!

--- app/src/main/java/com/github/quarck/calnotify/ui/ViewEventActivityNoRecents.kt
+++ app/src/main/java/com/github/quarck/calnotify/ui/ViewEventActivityNoRecents.kt
@@ -516,8 +516,8 @@ open class ViewEventActivityNoRecents : AppCompatActivity() {
 
     fun OnEnableMove(v: View?) {
         val btnEnable = findOrThrow<TextView>(R.id.enable_snooze_reschedule)
-        val layout1 = findOrThrow<TextView>(R.id.snooze_view_snooze_sub_layout_3)
-        val layout2 = findOrThrow<TextView>(R.id.snooze_view_snooze_sub_layout_4)
+        val layout1 = findOrThrow<LinearLayout>(R.id.snooze_view_snooze_sub_layout_3)
+        val layout2 = findOrThrow<LinearLayout>(R.id.snooze_view_snooze_sub_layout_4)
         btnEnable.visibility = View.GONE
         layout1.visibility = View.VISIBLE
         layout2.visibility = View.VISIBLE
